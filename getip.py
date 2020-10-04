b6a5f912a9209de8b9d337a626f46adcf55c9615
ymkymkymkymx <43044797+ymkymkymkymx@users.noreply.github.com>
2020-10-04 09:16:51 -0400

    testing

--- getip.py
+++ getip.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request
+import geoip2.database
+reader = geoip2.database.Reader('./GeoLite2-City.mmdb')
 app = Flask(__name__)
 @app.route('/')
 def index():
     ip = request.remote_addr
+    
     return render_template('index.html', user_ip=ip)
 if __name__ == '__main__':
     app.run(
---
+++ iptolocation.py
@@ -0,0 +1,2 @@
+import geoip2.database
+reader = geoip2.database.Reader('./GeoLite2-City.mmdb')
